dc2fd0e0cececb6a2ebe46e1cdc1da5bcbfc0b6a
Jan Nylund <jan.nylund@wartsila.com>
2018-12-01 14:36:36 +0200

    Day 01

---
+++ day01/day01.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+
+
+# Convert params to int
+def params_as_int(parameters):
+    return list(map(int, parameters))
+
+
+# Return the sum of the full array
+def sum_array(parameters):
+    return sum(params_as_int(parameters))
+
+
+# Return the first value we'll reach twice.
+def twice(parameters):
+    values = params_as_int(parameters)
+    pos = 0
+    result = 0
+    history = set()
+
+    while True:
+        pos = pos % len(values)
+        result = values[pos] + result
+
+        if result in history:
+            return result
+        else:
+            pos = pos + 1
+            history.add(result)
+
+
+if __name__ == "__main__":
+    with open('input.txt') as f:
+        params = f.readlines()
+    print("result day 01 part 1: ", sum_array(params))
+    print("result day 01 part 2: ", twice(params))
---
+++ day01/tests/test_day01.py
@@ -0,0 +1,19 @@
+from unittest import TestCase
+
+from day01.day01 import sum_array, twice
+
+
+class TestGame(TestCase):
+
+    def test_examples_part1(self):
+        self.assertEqual(3, sum_array("+1, -2, +3, +1".split(",")))
+        self.assertEqual(3, sum_array("+1, +1, +1".split(",")))
+        self.assertEqual(0, sum_array("+1, +1, -2".split(",")))
+        self.assertEqual(-6, sum_array("-1, -2, -3".split(",")))
+
+    def test_examples_part2(self):
+        self.assertEqual(0, twice("+1, -1".split(",")))
+        self.assertEqual(2, twice("+1, -2, +3, +1".split(",")))
+        self.assertEqual(10, twice("+3, +3, +4, -2, -4".split(",")))
+        self.assertEqual(5, twice("-6, +3, +8, +5, -6".split(",")))
+        self.assertEqual(14, twice("+7, +7, -2, -7, -4".split(",")))
